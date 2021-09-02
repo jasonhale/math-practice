adb547997f7c8e25f45c45d5e2ab92e38a18dd21
Jason Hale <paintmuncher@gmail.com>
2021-09-01 22:28:24 -0500

    Playing with Web Components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,145 @@
+class ProblemTemplate extends HTMLElement { 
+	constructor() {
+		super();
+		this.problemTypes = {
+			addition : '+',
+			subtraction : '-',
+			multiplication : 'x',
+			division : '/'
+		};	
+		this.attachShadow({ mode: 'open'});
+
+		this.wrapper = document.createElement('li');
+		this.wrapper.setAttribute('class', 'prob');
+		
+
+		this.label = document.createElement('label');
+		this.label.setAttribute('class', 'question');
+		
+		this.wrapper.appendChild(this.label);
+		
+		this.numA = document.createElement('span');
+		this.numA.setAttribute('class', 'num-a');
+		this.label.appendChild(this.numA);
+
+		this.op = document.createElement('span');
+		this.op.setAttribute('class', 'operator');
+		
+		// set svg
+		this.label.appendChild(this.op);
+
+		this.numB = document.createElement('span');
+		this.numB.setAttribute('class', 'num-b');
+		
+		this.label.appendChild(this.numB);
+
+		this.equals = document.createElement('span');
+		this.equals.setAttribute('class', 'equals');
+		this.equals.innerText = '=';
+		this.label.appendChild(this.equals);
+
+		this.answer = document.createElement('input');
+		this.answer.setAttribute('class', 'answer');
+		this.answer.setAttribute('type', 'text');
+		
+		this.wrapper.appendChild(this.answer);
+
+		const style = document.createElement('style');
+		style.textContent = `.prob {
+				font-weight: bold;
+				position: relative;
+			}
+			.question {
+				display: block;
+			}
+				.question span { display: inline-block; }
+				.question .num-a,
+				.question .num-b {
+					display: block;
+					text-align: right; 
+				}
+				.question .operator { position:absolute; }
+				.question .operator__svg {
+					width: var(--operandSize);
+					height: var(--operandSize);
+				}
+				.question .equals {
+					display: block;
+					height:0;
+					text-indent:-9999px;
+					border-bottom: solid 1px;
+				}
+			
+			.answer {
+				display: block;
+				width: 100%;
+				margin: 0;
+				padding: .2em 0;
+				font-size: 1em;
+				text-align: right;
+				border: 0 none;
+			}
+				.answer[data-solve*='0'],
+				.answer[data-solve*='1'],
+				.answer[data-solve*='2'],
+				.answer[data-solve*='3'],
+				.answer[data-solve*='4'],
+				.answer[data-solve*='5'],
+				.answer[data-solve*='6'],
+				.answer[data-solve*='7'],
+				.answer[data-solve*='8'],
+				.answer[data-solve*='9'] {
+					background-color: hsla(200,100%,5q0q%,.1);
+				}
+			
+				.answer.correct {
+					background-color: rgba(0,255,0,.3);
+				}
+				.answer.incorrect {
+					background-color: rgba(255,0,0,.3);
+				}
+				.answer.incorrect-fixed {
+					background-color: rgba(255,255,0,.5);
+				}
+		`;
+
+		this.shadowRoot.append(style, this.wrapper);
+	}
+
+	getAtts = () => {
+		const index = parseInt(this.getAttribute('i'), 10);
+		return {
+			numerator: this.getAttribute('num-a'),
+			denomenator: this.getAttribute('num-b'),
+			index,
+			problemtype: this.getAttribute('problem-type'),
+			labelFor: `problem${index}`,
+			answer: this.getAttribute('answer')
+		};
+	}
+
+	connectedCallback() {
+		const { numerator, denomenator, index, problemtype, labelFor, answer } = this.getAtts();
+		const wrapper = this.shadowRoot.querySelector('.prob');
+		wrapper.setAttribute('data-index', index + 1);
+		wrapper.setAttribute('data-problemtype', problemtype);
+		this.label.setAttribute('for', labelFor);
+		this.op.innerText = this.problemTypes[problemtype];
+		this.numA.innerText = numerator;
+		this.numB.innerText = denomenator;
+		this.answer.id = labelFor;
+		this.answer['tab-index'] = index;
+		this.answer.name = `answer${index}`;
+		this.answer['data-answer'] = answer;
+	}
+
+	attributeChangedCallback(e) {
+		console.log('attribute changed', e);
+	}
+}
+
+window.customElements.define('problem-li', ProblemTemplate);
+
 const _q = (q) => document.querySelector(q);
 const _qall = (q) => document.querySelectorAll(q);
 
@@ -117,18 +259,26 @@ var mt = {
 					break;
 			}
 
-			var template = _q('#problemTemplate').content.cloneNode(true);
-			template.querySelector('.prob').dataset.index = i + 1;
-			template.querySelector('.prob').dataset.problemtype = probtype;
-			template.querySelector('.question').setAttribute('for', 'problem'+i);
-			template.querySelector('.num-a').innerText = ''+numA;
-			template.querySelector('.num-b').innerText = ''+numB;
-			template.querySelector('.operator__svg use').setAttribute('href', mt.problemtypesmap[probtype]);
-			var answerNode = template.querySelector('.answer');
-			answerNode.setAttribute('id', 'problem'+i);
-			answerNode.setAttribute('tab-index', i);
-			answerNode.setAttribute('name', 'answer'+i);
-			answerNode.dataset.answer = answer;
+			// var template = _q('#problemTemplate').content.cloneNode(true);
+			// template.querySelector('.prob').dataset.index = i + 1;
+			// template.querySelector('.prob').dataset.problemtype = probtype;
+			// template.querySelector('.question').setAttribute('for', 'problem'+i);
+			// template.querySelector('.num-a').innerText = ''+numA;
+			// template.querySelector('.num-b').innerText = ''+numB;
+			// template.querySelector('.operator__svg use').setAttribute('href', mt.problemtypesmap[probtype]);
+			// var answerNode = template.querySelector('.answer');
+			// answerNode.setAttribute('id', 'problem'+i);
+			// answerNode.setAttribute('tab-index', i);
+			// answerNode.setAttribute('name', 'answer'+i);
+			// answerNode.dataset.answer = answer;
+
+			const template = document.createElement('problem-li');
+			console.log('template', template);
+			template.setAttribute('num-a', numA);
+			template.setAttribute('num-b', numB);
+			template.setAttribute('i', i);
+			template.setAttribute('problem-type', probtype);
+			template.setAttribute('answer', answer);
 			_q('.problems').appendChild(template);
 		};
 	},
